56678d71fc6d25cf07c3e4a477c97080a71584e0
robhog <robhogfeldt@gmail.com>
2015-11-29 15:53:08 +0100

    Trilers

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var matrix1 = {"Title":"matrix1","Year":"1999","Rated":"R","Released":"31 Mar 1999","Runtime":"136 min","Genre":"Action, Sci-Fi","Director":"Andy Wachowski, Lana Wachowski","Writer":"Andy Wachowski, Lana Wachowski","Actors":"Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving","Plot":"A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.","Language":"English","Country":"USA, Australia","Awards":"Won 4 Oscars. Another 33 wins & 40 nominations.","Poster":"http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX300.jpg","Metascore":"73","imdbRating":"8.7","imdbVotes":"1,119,993","imdbID":"tt0133093","Type":"movie","Response":"True", "Oscars": ["Best Film Editing","Best Sound","Best Effects, Sound Effects Editing","Best Effects, Visual Effects"]}
+var matrix1 = {"Title":"matrix1","Year":"1999","Rated":"R","Released":"31 Mar 1999","Runtime":"136 min","Genre":"Action, Sci-Fi","Director":"Andy Wachowski, Lana Wachowski","Writer":"Andy Wachowski, Lana Wachowski","Actors":"Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving","Plot":"A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.","Language":"English","Country":"USA, Australia","Awards":"Won 4 Oscars. Another 33 wins & 40 nominations.","Poster":"http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX300.jpg","Metascore":"73","imdbRating":"8.7","imdbVotes":"1,119,993","imdbID":"tt0133093","Type":"movie","Response":"True", "Oscars": ["Best Film Editing","Best Sound","Best Effects, Sound Effects Editing","Best Effects, Visual Effects"], "Trailer":"https://www.youtube.com/embed/_Ls19O-9p3s"}
 
 
-var matrix2 = {"Title":"matrix2","Year":"2003","Rated":"R","Released":"15 May 2003","Runtime":"138 min","Genre":"Action, Sci-Fi","Director":"Andy Wachowski, Lana Wachowski","Writer":"Andy Wachowski, Lana Wachowski, Andy Wachowski (characters), Lana Wachowski (characters)","Actors":"Ray Anthony, Christine Anu, Andy Arness, Alima Ashton-Sheibu","Plot":"Neo and the rebel leaders estimate that they have 72 hours until 250,000 probes discover Zion and destroy it and its inhabitants. During this, Neo must decide how he can save Trinity from a dark fate in his dreams.","Language":"English, French","Country":"USA, Australia","Awards":"5 wins & 21 nominations.","Poster":"http://ia.media-imdb.com/images/M/MV5BMjA0NDM5MDY2OF5BMl5BanBnXkFtZTcwNzg5OTEzMw@@._V1_SX300.jpg","Metascore":"62","imdbRating":"7.2","imdbVotes":"400,966","imdbID":"tt0234215","Type":"movie","Response":"True","Oscars": ["Best Science Fiction Film(Nominated)","Best Costumes(Nominated)"]}
+var matrix2 = {"Title":"matrix2","Year":"2003","Rated":"R","Released":"15 May 2003","Runtime":"138 min","Genre":"Action, Sci-Fi","Director":"Andy Wachowski, Lana Wachowski","Writer":"Andy Wachowski, Lana Wachowski, Andy Wachowski (characters), Lana Wachowski (characters)","Actors":"Ray Anthony, Christine Anu, Andy Arness, Alima Ashton-Sheibu","Plot":"Neo and the rebel leaders estimate that they have 72 hours until 250,000 probes discover Zion and destroy it and its inhabitants. During this, Neo must decide how he can save Trinity from a dark fate in his dreams.","Language":"English, French","Country":"USA, Australia","Awards":"5 wins & 21 nominations.","Poster":"http://ia.media-imdb.com/images/M/MV5BMjA0NDM5MDY2OF5BMl5BanBnXkFtZTcwNzg5OTEzMw@@._V1_SX300.jpg","Metascore":"62","imdbRating":"7.2","imdbVotes":"400,966","imdbID":"tt0234215","Type":"movie","Response":"True","Oscars": ["Best Science Fiction Film(Nominated)","Best Costumes(Nominated)"],"Trailer":"https://www.youtube.com/embed/kYzz0FSgpSU"}
 	
-var matrix3 = {"Title":"matrix3","Year":"2003","Rated":"R","Released":"05 Nov 2003","Runtime":"129 min","Genre":"Action, Sci-Fi","Director":"Andy Wachowski, Lana Wachowski","Writer":"Andy Wachowski, Lana Wachowski, Andy Wachowski (characters), Lana Wachowski (characters)","Actors":"Mary Alice, Tanveer K. Atwal, Helmut Bakaitis, Kate Beahan","Plot":"The human city of Zion defends itself against the massive invasion of the machines as Neo fights to end the war at another front while also opposing the rogue Agent Smith.","Language":"English, French","Country":"Australia, USA","Awards":"1 win & 19 nominations.","Poster":"http://ia.media-imdb.com/images/M/MV5BMTkyNjc4NTQzOV5BMl5BanBnXkFtZTcwNDYzMTQyMQ@@._V1_SX300.jpg","Metascore":"47","imdbRating":"6.7","imdbVotes":"346,296","imdbID":"tt0242653","Type":"movie","Response":"True","Oscars": ["NO AWARDS(piece of crap)"]}	
+var matrix3 = {"Title":"matrix3","Year":"2003","Rated":"R","Released":"05 Nov 2003","Runtime":"129 min","Genre":"Action, Sci-Fi","Director":"Andy Wachowski, Lana Wachowski","Writer":"Andy Wachowski, Lana Wachowski, Andy Wachowski (characters), Lana Wachowski (characters)","Actors":"Mary Alice, Tanveer K. Atwal, Helmut Bakaitis, Kate Beahan","Plot":"The human city of Zion defends itself against the massive invasion of the machines as Neo fights to end the war at another front while also opposing the rogue Agent Smith.","Language":"English, French","Country":"Australia, USA","Awards":"1 win & 19 nominations.","Poster":"http://ia.media-imdb.com/images/M/MV5BMTkyNjc4NTQzOV5BMl5BanBnXkFtZTcwNDYzMTQyMQ@@._V1_SX300.jpg","Metascore":"47","imdbRating":"6.7","imdbVotes":"346,296","imdbID":"tt0242653","Type":"movie","Response":"True","Oscars": ["NO AWARDS(piece of crap)"],"Trailer":"https://www.youtube.com/embed/hMbexEPAOQI"}	
 
 //Vanilla
 // onload = function(){
@@ -50,40 +50,6 @@ $.each(matrixArr, function( i, l ){
 }
 
 
-
-
-// function animateChoosenMatrix(choosenOne){
-// 	var choosen;
-//  if ($(".stashed")[0]){
-//     // Do something if class exists
-// } else {
-//     // Do something if class does not exist
-//     $(".row").append("<div class='col-md-3  choosenMatrix'><img/></div>");
-//     $(".row").append("<div class='col-md-3 col-md-offset-5 stashed'></div>");
-// }
-  
-// 	$.each(matrixArr, function( i, l ){
-    
-//   	if(l.Title!==choosenOne.Title){	  
-//      var img = l.Poster; 
-//      choosen = l.Title;
-//     $( ".stashed" ).append("<img id="+choosen+" class='myClass' src="+ img +"/>"); 
-// 	  $( "#" + l.Title ).remove("#" + l.Title)
-//     } else {
-//     choosen = l.Title;
-    
-    
-//     $( ".choosenMatrix img" ).attr('src', l.Poster); 
-    
-    
-// 		$( "#" + l.Title ).append( "<p> Year: "+ l.Year +"</p>" ).append( "<p> Plot: "+ l.Plot+"</p>" );
-//     $(".page-header").append($("<button onClick=showAwards("+ choosen +")>Awards</button>"));
-//     $(".page-header").find("h1").html(l.Title);
-    
-// 	} 
-//   }); 
-// }
-
 function animateChoosenMatrix(choosenOne){
 
  if ($(".stashed")[0]){
@@ -91,8 +57,9 @@ function animateChoosenMatrix(choosenOne){
     $( ".stashed" ).empty();
 } else {
     // Do something if class does not exist
-    $(".row").append("<div class='col-md-3  choosenMatrix'><img/></div>");
-    $(".row").append("<div class='col-md-3 col-md-offset-5 stashed'></div>");
+    $(".row").append("<div class='col-md-3 choosenMatrix'><img/></div>");
+    $(".row").append("<div class='col-md-6 middle'></div>");
+    $(".row").append("<div class='col-md-2 col-md-offset-1 stashed'></div>");
 }
   
 	$.each(matrixArr, function( i, l ){
@@ -114,75 +81,57 @@ function animateChoosenMatrix(choosenOne){
 }
 
 function updateHeader(l){
-  var matrix = matrix;
+  
   	$( "#" + l.Title ).append( "<p> Year: "+ l.Year +"</p>" ).append( "<p> Plot: "+ l.Plot+"</p>" );
     $("button").remove();
-    $(".page-header").append($("<button name="+ l.Title +" class='awards'>Awards</button>")).append($("<button name="+ l.Plot +" class='plot'>Plot</button>"));
+    $(".page-header").append($("<button name="+ l.Title +" class='awards btn-success'>Awards</button>"))
+                     .append($("<button name="+ l.Title +" class='plot btn-success'>Plot</button>"))
+                     .append($("<button name="+ l.Title +" class='trailer btn-success''>Trailer</button>"));
     $(".page-header").find("h1").html(l.Title);
-  
 }
 
-var showAwards = function(choosen){
-//  var ul = $( "#" + choosen.Title).removeClass("col-md-4").addClass("col-md-6").prepend('<ul class="awards"></ul>').find('ul');
-// var ul = $( "#" + choosen.Title).append('<ul class="awards"></ul>').find('ul');
-// var img = $("#" + choosen.Title ).find('img');
-// $('<ul class="awards"></ul>').insertAfter(img);
-// var ul = $( "#" + choosen.Title).find('ul');
-//  $.each(choosen.Oscars, function( i, l ){
-//   ul.append("<li>"+ l +"</li>").slideDown("slow");
-// })
-
-console.log(this);
-}
-
-
-
-
-
+//Choose from stashed
 $('body').on('click', '.myClass', function() {
-  //do something
   $(".movieAwards").remove();
+  $(".movietrailer").remove();
   console.log(this.name);
-  myMatrix(this.name);
+  var matrix = getMatrix(this.name)
+  animateChoosenMatrix(matrix)
 });
 
 // Awards
 $('body').on('click', '.awards', function() {
   var matrix = getMatrix(this.name);
+    $(".movietrailer").remove();
+    $(".movieplot").remove();
+    $(".awardsList").remove();
+    $(".middle").append($("<div class='movieAwards'><ul class='awardsList'></ul></div>"));
     
-    $(".choosenMatrix").after($("<div class='col-md-3 movieAwards'><ul class='awardsList'></ul></div>"));
     $.each(matrix.Oscars, function( i, l ){
-    $(".awardsList").append("<li>"+ l +"</li>").hide().animate({
-    width: [ "toggle", "swing" ],
-    height: [ "toggle", "swing" ],
-    opacity: "toggle"
-  }, 300, "linear")
+    $(".awardsList").append("<li>"+ l +"</li>");
   })
 });
 
 // Plot
 $('body').on('click', '.plot', function() {
-  var matrix = getMatrix(this.name);
-    
-    $(".choosenMatrix").after($("<div class='col-md-3 movieAwards'><ul class='awardsList'></ul></div>"));
-    $.each(matrix.Oscars, function( i, l ){
-    $(".awardsList").append("<li>"+ l +"</li>").hide().animate({
-    width: [ "toggle", "swing" ],
-    height: [ "toggle", "swing" ],
-    opacity: "toggle"
-  }, 300, "linear")
-  })
+   $(".movietrailer").remove();
+    $(".movieplot").remove();
+    $(".awardsList").remove();
+    var matrix = getMatrix(this.name);  
+    $(".middle").append($("<div class='movieplot'>"+ matrix.Plot +"</div>"));
 });
 
-var myMatrix = function(ev){	
-$.each(matrixArr, function( i, l ){
-  if(l.Title==ev){	  
-	animateChoosenMatrix(l)
-  }})
-  }
+// Trailer
+$('body').on('click', '.trailer', function() {
   
+    $(".movieplot").remove();
+    $(".awardsList").remove();
+    $(".movietrailer").remove();
+    var matrix = getMatrix(this.name);  
+    $(".middle").append($("<iframe width='420' height='315' src="+matrix.Trailer+" class='movietrailer'></iframe>"));
+});
 
-
+// Get Matrix by Title
 function getMatrix(matrix)
 {  
    var toReturn; 
@@ -195,5 +144,3 @@ function getMatrix(matrix)
    }); 
    return toReturn; 
 }
-
-
